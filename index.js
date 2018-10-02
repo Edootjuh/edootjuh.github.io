2ab7664c3d723a3f64698ce63bd538c434fae622
Edo Mangelaars <edo@edo.codes>
2018-10-02 16:49:15 +0200

    GitLab link. Tweaks

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-if (document.location.search && document.location.search.indexOf('sent') !== -1)
-    document.getElementById('div').innerHTML = `
-    <h1>Edo Mangelaars</h1>
+if (document.location.search && document.location.search.indexOf("sent") !== -1)
+  document.getElementById("emailform").innerHTML = `
     <h3 id="thanks">Thanks</h3>`;
 else {
-    document.getElementById('div').innerHTML = `
-<h1>Edo Mangelaars</h1>
-<div id="bottom">
-    <a id="linkedin" href="//www.linkedin.com/in/edo-mangelaars-88649555/">in</a>
+  document.getElementById("emailform").innerHTML = `
     <form id="form" action="//formcarry.com/s/HJUO3Yirf" method="POST" accept-charset="UTF-8">
         <p id="emailme">E-mail me</p>
         <input id="email" type="email" name="email" placeholder="Your e-mail address" required>
@@ -16,33 +12,31 @@ else {
                 data-callback="onSubmit">
             Send
         </button>
-    </form>
-</div>`;
-    window.addEventListener('load', checkValidity);
-    const script = document.createElement('script');
-    script.src = '//www.google.com/recaptcha/api.js?onload=onLoad';
-    document.body.appendChild(script);
+    </form>`;
+  window.addEventListener("load", checkValidity);
+  const script = document.createElement("script");
+  script.src = "//www.google.com/recaptcha/api.js?onload=onLoad";
+  document.body.appendChild(script);
 }
 
 function checkValidity() {
-    const email = document.getElementById('email');
-    const message = document.getElementById('message');
-    const send = document.getElementById('send');
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+  const send = document.getElementById("send");
 
-    const valid = !(email.validity) || (email.validity.valid && message.validity.valid);
-    if (valid) send.removeAttribute('disabled');
-    else send.setAttribute('disabled', '');
-    return valid;
+  const valid =
+    !email.validity || (email.validity.valid && message.validity.valid);
+  if (valid) send.removeAttribute("disabled");
+  else send.setAttribute("disabled", "");
+  return valid;
 }
 
 function onLoad() {
-    document.getElementById('email').addEventListener('input', checkValidity);
-    document.getElementById('message').addEventListener('input', checkValidity);
-    setTimeout(checkValidity, 0);
+  document.getElementById("email").addEventListener("input", checkValidity);
+  document.getElementById("message").addEventListener("input", checkValidity);
+  setTimeout(checkValidity, 0);
 }
 
 function onSubmit(token) {
-    if (checkValidity())
-        document.getElementById('form').submit();
+  if (checkValidity()) document.getElementById("form").submit();
 }
-
